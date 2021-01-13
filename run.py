84243635bbea2341a09dd147409a51d1069e14e6
MR404cyber <75874865+MR404cyber@users.noreply.github.com>
2021-01-14 00:51:36 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import sys
 if sys.version_info.major != 3:
 	exit("{}[!] Kamu Menggunakan Python Versi {}\n[!] Harap Gunakan Python Versi 3\n[+] Silahkan Untuk Mengketikan python3 {}".format("\x1b[1;31m",sys.version_info.major,sys.argv[0]))
 if __name__=="__main__":
-	from 404 import menu
+	from piton3 import menu
 	menu()
