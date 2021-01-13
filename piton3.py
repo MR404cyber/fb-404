636b2ca28b674f4c773f103640881fe5fc9877f7
MR404cyber <75874865+MR404cyber@users.noreply.github.com>
2021-01-14 01:15:53 +0800

    Update piton3.py

--- piton3.py
+++ piton3.py
@@ -33,7 +33,6 @@ def ingfo():
 def milih():
 	bersih()
 	echo("+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+")
-        echo("Author MR.404")
 	echo("Youtube MR.404")
         echo("+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+")
         echo("[01] Login With Token")
